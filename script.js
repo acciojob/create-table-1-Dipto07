e5adb59267a1f8ee042dc2d7c523b0680ddca52d
Dipto Neogi <81651700+Dipto07@users.noreply.github.com>
2024-04-30 15:09:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function insert_Row() {
     //Write your code here
+	 const table = document.getElementById('sampleTable');
+    
+    const newRow = document.createElement('tr');
+    
+    const leftCell = document.createElement('td');
+    const rightCell = document.createElement('td');
+    leftCell.textContent = 'New Cell1';
+    rightCell.textContent = 'New Cell2';
+    newRow.appendChild(leftCell);
+    newRow.appendChild(rightCell);
+    
+    table.insertBefore(newRow, table.firstChild);
   
   
 }
